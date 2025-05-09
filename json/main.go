90857a4281a497f3c8c5c900d16b2265f2454e2e
TanishMoral11 <ishantmoral@gmail.com>
2025-05-10 03:17:19 +0530

    json

---
+++ json/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	Name string `json:"name"`
+	Age int `json:"age"`
+	IsAdult bool `json:"is_adult"`
+}
+func main() {
+	fmt.Println("We Are Learning JSON in Go")
+
+	person := Person{
+		Name: "John Doe",
+		Age: 30,
+		IsAdult: true,
+	}
+
+	fmt.Println("Person Data : ", person)
+
+	//Convert person into JSON Encoding (Marshalling)
+
+	jsonData, err := json.Marshal(person)
+	if err != nil {
+		fmt.Println("Error Marshalling JSON: ", err)
+		return
+	}
+
+	fmt.Println("Json Data : ", string(jsonData))
+
+	// Decoding (Unmarshalling)
+
+	var personData Person
+	err = json.Unmarshal(jsonData, &personData)
+	if err != nil {
+		fmt.Println("Error Unmarshalling JSON: ", err)
+		return
+	}
+
+	fmt.Println("Unmarshalled Data : ", personData)
+}
