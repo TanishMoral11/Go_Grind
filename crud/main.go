4dae93311dca461faf0ea2712b1155f08adc4adf
TanishMoral11 <ishantmoral@gmail.com>
2025-06-03 15:43:47 +0530

    DELETE

--- crud/main.go
+++ crud/main.go
@@ -85,10 +85,56 @@ func performPostRequest() {
 	fmt.Println("Resonse ", string(data))
 }
 
+// func performUpdateRequest() {
+// 	todo := Todo{
+// 		UserID: 70,
+// 		Title: "Tanish Moral Golang Hello World",
+// 		Completed: false,
+// 	}
+
+// 	// Convert The Todo struct to JSON
+// 	jsonData, err := json.Marshal(todo)
+// 	if err != nil {
+// 		fmt.Println("Error converting to JSON:", err)
+// 		return
+// 	}
+
+// 	const myUrl = "https://jsonplaceholder.typicode.com/todos/1"
+
+// 	// Create PUT request
+// 	http.NewRequest()
+// }
+
+func performDeleteRequest(){
+	const myUrl = "https://jsonplaceholder.typicode.com/todos/1"
+
+	// Create a new DELETE request
+
+	// here we pass nil , as we did not sends any data in the body of the request
+	req, err := http.NewRequest(http.MethodDelete, myUrl, nil)
+	if err != nil {
+		fmt.Println("Error creating DELETE request:", err)
+		return
+	}
+
+	// send the request
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending DELETE request:", err)
+		return
+	}
+	defer res.Body.Close()
+	fmt.Println("Response Status Code : ", res.Status)
+}
+
+
+
 
 func main() {
 	fmt.Println("Learning Go with CRUD operations\n")
 	// performGetRequest()
-	performPostRequest()
+	// performPostRequest()
+	performDeleteRequest()
 
 }
